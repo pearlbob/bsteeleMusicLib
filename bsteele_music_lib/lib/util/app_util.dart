c15ed13c9403a7a9d7a94148ecdb4b4a70772745
pearlbob <bob@bsteele.com>
2025-10-23 17:13:09 -0700

    app_util.dart added

---
+++ bsteele_music_lib/lib/util/app_util.dart
@@ -0,0 +1,25 @@
+//  note: keep this file dart only!
+
+String to12(final double v, {int pad = 10}) {
+  return v.toStringAsFixed(12).padLeft(pad);
+}
+
+String to9(final double v, {int pad = 10}) {
+  return v.toStringAsFixed(9).padLeft(pad);
+}
+
+String to6(final double v, {int pad = 10}) {
+  return v.toStringAsFixed(6).padLeft(pad);
+}
+
+String to3(final double v, {int pad = 8}) {
+  return v.toStringAsFixed(3).padLeft(pad);
+}
+
+String to1(final double v, {int pad = 5}) {
+  return v.toStringAsFixed(1).padLeft(pad);
+}
+
+String to0(final double v, {int pad = 5}) {
+  return v.toStringAsFixed(0).padLeft(pad);
+}
